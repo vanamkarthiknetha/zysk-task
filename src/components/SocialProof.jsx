9a645d70866eda02f93c4a1b290ff4854625f77d
vanamkarthiknetha <vanamkarthiknetha@gmail.com>
2025-01-18 16:55:35 +0530

    mods in social proof

--- src/components/SocialProof.jsx
+++ src/components/SocialProof.jsx
@@ -4,13 +4,13 @@ const SocialProof = () => {
     <section className="max-w-[90%] md:max-w-[85%] mx-auto pb-24  border-b-2 border-b-slate-100 mb-20">
         <div id="container" className="flex flex-col items-center">
             <p className="py-8 text-md text-[#475467] font-[500]">Join 4,000+ companies already growing</p>
-            <div id="logos" className="grid grid-cols-2 md:flex justify-between w-full gap-6 md:gap-0">
-                <img src="Socialproof/Fictional company logo.png" alt=""     className="h-12 "/>
-                <img src="Socialproof/Fictional company logo (1).png" alt="" className="h-12 "/>
-                <img src="Socialproof/Fictional company logo (2).png" alt="" className="h-12 "/>
-                <img src="Socialproof/Fictional company logo (3).png" alt="" className="h-12 "/>
-                <img src="Socialproof/Fictional company logo (4).png" alt="" className="h-12 "/>
-                <img src="Socialproof/Fictional company logo (5).png" alt="" className="h-12 "/>
+            <div id="logos" className="grid grid-cols-2 lg:flex w-full gap-6 lg:gap-0">
+                <img src="Socialproof/Fictional company logo.png" alt=""     className="h-12 mx-auto"/>
+                <img src="Socialproof/Fictional company logo (1).png" alt="" className="h-12 mx-auto"/>
+                <img src="Socialproof/Fictional company logo (2).png" alt="" className="h-12 mx-auto"/>
+                <img src="Socialproof/Fictional company logo (3).png" alt="" className="h-12 mx-auto"/>
+                <img src="Socialproof/Fictional company logo (4).png" alt="" className="h-12 mx-auto"/>
+                <img src="Socialproof/Fictional company logo (5).png" alt="" className="h-12 mx-auto"/>
             </div>
         </div>
       
