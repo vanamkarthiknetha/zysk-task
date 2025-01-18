ad33aea9442b9c42df83113dd0bdb60e3b52a9fa
vanamkarthiknetha <vanamkarthiknetha@gmail.com>
2025-01-18 17:35:14 +0530

    adjust for tabs

--- src/components/HeadingBadge.jsx
+++ src/components/HeadingBadge.jsx
@@ -3,7 +3,7 @@ const HeadingBadge = () => {
     <div id="headingBadge" className="flex flex-col mx-auto items-center">
       <div
         id="badge"
-        className="flex justify-around py-1 px-1 md:px-0 rounded-full  border border-[#E9D7FE] bg-[#F9F5FF] md:border-[#E63F3A]/30 md:bg-[#E63F3A]/[.03] w-full md:w-[50%]"
+        className="flex justify-around py-1 px-1 md:px-0 rounded-full  border border-[#E9D7FE] bg-[#F9F5FF] md:border-[#E63F3A]/30 md:bg-[#E63F3A]/[.03] w-full md:w-[80%] xl:w-[50%]"
       >
         <div className="flex rounded-full border border-[#D6BBFB] md:border-[#e6403ab0] bg-white  w-[30%] md:w-[28%] ">
           <p className="text-[#E63F3A] font-[500] text-md mx-auto">New feature</p>
